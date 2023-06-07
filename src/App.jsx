914e9892fa36b1d2b917514e7155f86a0cd4f5c3
guy tsabari <guytsabari123@gmail.com>
2023-06-07 12:46:33 +0300

    0.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import './App.css'
+import './components/Layout/Layout'
+import Home from './components/Home/Home';
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <>
-    
-    </>
+    <div>
+
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home/>}></Route>
+          <Route path='contact' element={<Contact />}></Route>
+          <Route path='about' element={<About />}></Route>
+          <Route path='challenges' element={<Challenges/>}></Route>
+          <Route path='profile' element={<Profile/>}></Route>
+        
+        </Route>
+      </Routes>
+
+
+
+    </div>
   )
 }
 
